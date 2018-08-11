3a8d92a72fb873151552136e03577c1d4a84eb4c
Divum <jitendra@divum.in>
2018-08-11 22:16:59 +0530

    Added impl

--- html/js/fed-res-bank.js
+++ html/js/fed-res-bank.js
@@ -2,12 +2,15 @@ function onWeb3Create(){
     initFedResBank();
 }
 function initFedResBank(){
-    var fedResBankABI = SOPweb3.eth.contract(fedResSys.abi);
+    var fedResBankABI = web3.eth.contract(fedResSys.abi);
     frsContract = fedResBankABI.at(FRS_CONTRACT_ADDRESS);
 }
 function printMoney(){
-    frsContract.printMoney.estimateGas(rid, buyerID, function(error, _gas){
-        
+    var denomination = parseInt($("#denomination").val());
+    var quantity = parseInt($("#quantity").val());
+    var _from = web3.eth.accounts[0];
+    frsContract.printMoney.sendTransaction(denomination, quantity, {from:_from}, function(error, _gas){
+        debugger;
     });
 
 }
